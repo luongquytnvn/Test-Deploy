b2c0931099b8e9bf8763caa4a1fb83fe5b8c032f
luongquytnvn <luongquytnvn@gmail.com>
2020-02-12 02:34:24 +0700

    fix

---
+++ src/main/java/com/codegym/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.codegym.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class TestController {
+    @GetMapping("")
+    public String test() {
+        return "Ok";
+    }
+}
